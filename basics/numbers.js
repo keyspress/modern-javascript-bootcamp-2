645ea267e9b78eb52d787687e9924ad452a3ed7c
Kyle Desktop <kylespresser@gmail.com>
2018-09-19 22:21:45 -0400

    lecture 11

---
+++ basics/numbers.js
@@ -0,0 +1,17 @@
+// let num = 5.5 + 2;
+// let num = 5.5 - 2;
+// let num = 5.5 - -1;
+// let num = 5.5 * 2;
+// let num = 5.5 / 2;
+// let x = 11;
+// let num = (x + 1) * 2;
+// let age = 42;
+// let dogYears = (age + 1) / 7;
+
+// console.log(dogYears);
+
+let studentScore = 17;
+let maxScore = 20;
+let percent = (studentScore / maxScore) * 100;
+
+console.log(`${percent}%`);
---
+++ basics/strings.js
@@ -0,0 +1,14 @@
+// let name = 'Kyle Spresser';
+
+// let firstName = 'Kyle';
+// let lastName = 'Spresser';
+
+// let fullName = firstName + ' ' + lastName;
+
+// console.log(`My name is ${firstName} ${lastName} and ${fullName}`);
+
+let city = 'Lakeland';
+let country = 'USA';
+let location = city + ', ' + country;
+
+console.log(location);
