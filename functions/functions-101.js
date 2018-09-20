89e963b698ee3722b8546618fbcb72080f14439a
Kyle Desktop <kylespresser@gmail.com>
2018-09-20 10:33:57 -0400

    lecture 21

---
+++ functions/functions-101.js
@@ -0,0 +1,18 @@
+// let greetUser = function() {
+//   console.log('So whatcha whatcha whatcha want?');
+// };
+
+// greetUser();
+
+// let squareNumber = function(num) {
+//   console.log(num * num);
+// };
+
+// squareNumber(4);
+
+let convertFahrenheitToCelcius = function(temp) {
+  return ((temp - 32) * 5) / 9;
+};
+
+console.log(convertFahrenheitToCelcius(32));
+console.log(convertFahrenheitToCelcius(68));
