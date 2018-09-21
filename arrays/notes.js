681d4265eef07cceea82e1b7b0b7e06fdfda905b
Kyle Desktop <kylespresser@gmail.com>
2018-09-21 11:32:06 -0400

    lecture 39

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,5 +1,15 @@
 const notes = ['Note 1', 'Note 2', 'Note 3'];
 
+// console.log(notes.pop());
+// notes.push('Note 4');
+
+// console.log(notes.shift());
+// notes.unshift('First note');
+
+// notes.splice(1, 0, 'This comes second');
+
+notes[2] = 'New note 3';
+
 console.log(notes.length);
 
-console.log(notes[notes.length - 1]);
+console.log(notes);
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,4 +1,8 @@
 const todos = ['Get milk', 'Drink milk', 'Get more milk'];
 
+todos.splice(2, 1);
+todos.push('Steal milk');
+todos.shift();
+
 console.log(`You have ${todos.length} todos.`);
-console.log(`Todo: ${todos[0]}, Todo: ${todos[todos.length - 1]}`);
+console.log(todos);
