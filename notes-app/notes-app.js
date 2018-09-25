9532d1861515543396212a55ee9bdb2bca8e90e4
Kyle Desktop <kylespresser@gmail.com>
2018-09-25 11:11:56 -0400

    lecture 52

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,3 +1,18 @@
+const notes = [
+  {
+    title: 'My next trip',
+    body: 'I want to go to Hawaii'
+  },
+  {
+    title: 'Habit to work on',
+    body: 'Sleep less'
+  },
+  {
+    title: 'Office modification',
+    body: 'Get a new chair'
+  }
+];
+
 // const p = document.querySelector('p');
 // p.remove();
 
@@ -6,3 +21,7 @@ const ps = document.querySelectorAll('p');
 // ps.forEach(p => p.remove());
 // ps.forEach(p => console.log(p.textContent));
 ps.forEach(p => console.log((p.textContent = '********')));
+
+const newParagraph = document.createElement('p');
+newParagraph.textContent = 'This is a brand spankin new element!';
+document.querySelector('body').appendChild(newParagraph);
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,45 @@
-const ps = document.querySelectorAll('p');
-
-ps.forEach(p => {
-  if (p.textContent.includes('the')) {
-    p.remove();
+const todos = [
+  {
+    text: 'Get milk',
+    completed: true
+  },
+  {
+    text: 'Drink milk',
+    completed: true
+  },
+  {
+    text: 'Cry over having no milk',
+    completed: false
+  },
+  {
+    text: 'Get more milk',
+    completed: false
+  },
+  {
+    text: 'Go to Disney World',
+    completed: false
   }
+];
+
+const incompleteTodos = todos.filter(todo => {
+  return !todo.completed;
+});
+
+// const incomplete = [];
+
+// todos.forEach(todo => {
+//   if (todo.completed === false) {
+//     incomplete.push(todo);
+//   }
+// });
+
+const summary = document.createElement('h2');
+
+summary.textContent = `You have ${incompleteTodos.length} todos left`;
+document.querySelector('body').appendChild(summary);
+
+todos.forEach(todo => {
+  const p = document.createElement('p');
+  p.textContent = todo.text;
+  document.querySelector('body').appendChild(p);
 });
