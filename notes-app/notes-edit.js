4745dafcfb8ddcde763e18035370d69371df6236
Kyle Desktop <kylespresser@gmail.com>
2018-09-26 21:18:53 -0400

    lecture 73

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -2,8 +2,8 @@ const titleElement = document.querySelector('#note-title');
 const bodyElement = document.querySelector('#note-body');
 const removeElement = document.querySelector('#remove-note');
 const noteId = location.hash.substring(1);
-const notes = getSavedNotes();
-const note = notes.find(note => {
+let notes = getSavedNotes();
+let note = notes.find(note => {
   return note.id === noteId;
 });
 
@@ -28,4 +28,20 @@ removeElement.addEventListener('click', e => {
   removeNote(note.id);
   saveNotes(notes);
   location.assign('http://127.0.0.1:5500/notes-app/index.html');
+});
+
+window.addEventListener('storage', e => {
+  if (e.key === 'notes') {
+    notes = JSON.parse(e.newValue);
+    note = notes.find(note => {
+      return note.id === noteId;
+    });
+
+    if (note === undefined) {
+      location.assign('http://127.0.0.1:5500/notes-app/index.html');
+    }
+
+    titleElement.value = note.title;
+    bodyElement.value = note.body;
+  }
 });
