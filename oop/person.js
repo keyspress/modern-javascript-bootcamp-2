ed55b40e37b3df9617de644582e89402dda703a3
Kyle Desktop <kylespresser@gmail.com>
2018-09-28 12:49:11 -0400

    lecture 92

--- oop/person.js
+++ oop/person.js
@@ -1,14 +1,29 @@
-const Person = function(firstName, lastName, age) {
+const Person = function(firstName, lastName, age, likes = []) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.age = age;
+  this.likes = likes;
 };
 
-const me = new Person('Bill', 'Bond', 33);
-me.firstName = 'Billy';
+Person.prototype.getBio = function() {
+  let bio = `${this.firstName} is ${this.age}.`;
 
-console.log(me);
+  this.likes.forEach(like => {
+    bio += ` ${this.firstName} likes ${like}.`;
+  });
+
+  return bio;
+};
+
+Person.prototype.setName = function(fullName) {
+  const names = fullName.split(' ');
+  this.firstName = names[0];
+  this.lastName = names[1];
+};
+
+const me = new Person('Bill', 'Bond', 33, ['Eating', 'Sleeping']);
+me.setName('Jimmy Jones');
+console.log(me.getBio());
 
 const person2 = new Person('Jim Bob', 'Dingy', 62);
-
-console.log(person2);
+console.log(person2.getBio());
