a315616463540d169a5c46a369756fa284f936e1
Kyle Desktop <kylespresser@gmail.com>
2018-10-02 12:59:41 -0400

    lecture 105

--- hangman/app.js
+++ hangman/app.js
@@ -16,11 +16,13 @@ window.addEventListener('keypress', e => {
 const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', e => {
-  if (e.target.readyState === 4) {
+  if (e.target.readyState === 4 && e.target.status === 200) {
     const data = JSON.parse(e.target.responseText);
     console.log(data);
+  } else if (e.target.readyState === 4) {
+    console.log('error');
   }
 });
 
-request.open('GET', 'http://puzzle.mead.io/puzzle');
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3');
 request.send();
