16296b802f1e4d9c6b59155ee981f67d986d66d1
Kyle Desktop <kylespresser@gmail.com>
2018-10-02 09:12:14 -0400

    lecture 97 challenge 2 teacher solution

--- hangman/app.js
+++ hangman/app.js
@@ -1,46 +1,14 @@
-Hangman.prototype.getPuzzle = function() {
-  let puzzle = '';
-  this.word.forEach(letter => {
-    if (this.guessedLetters.includes(letter) || letter === ' ') {
-      puzzle += letter;
-    } else {
-      puzzle += '*';
-    }
-  });
-  return puzzle;
-};
-
-Hangman.prototype.makeGuess = function(guess) {
-  guess = guess.toLowerCase();
-  const isUnique = !this.guessedLetters.includes(guess);
-  const isBadGuess = !this.word.includes(guess);
-
-  if (isUnique) {
-    this.guessedLetters.push(guess);
-  }
-  if (isUnique && isBadGuess) {
-    this.remainingGuesses--;
-  }
-};
-
+const puzzleEl = document.querySelector('#puzzle');
+const guessesEl = document.querySelector('#guesses');
 const game1 = new Hangman('dog', 3);
 
-const generatePuzzleDOM = () => {
-  const puzzleEl = document.createElement('h2');
-  const remainingGuessEl = document.createElement('h4');
-  puzzleEl.textContent = game1.getPuzzle();
-  remainingGuessEl.textContent = game1.remainingGuesses;
-  document.querySelector('#puzzle').appendChild(puzzleEl);
-  document.querySelector('#puzzle').appendChild(remainingGuessEl);
-};
-
-generatePuzzleDOM();
+puzzleEl.textContent = game1.getPuzzle();
+guessesEl.textContent = game1.remainingGuesses;
 
 window.addEventListener('keypress', e => {
   const guess = String.fromCharCode(e.charCode);
   game1.makeGuess(guess);
   document.querySelector('#puzzle').innerHTML = '';
-  generatePuzzleDOM();
-  // console.log(game1.getPuzzle());
-  // console.log(game1.remainingGuesses);
+  puzzleEl.textContent = game1.getPuzzle();
+  guessesEl.textContent = game1.remainingGuesses;
 });
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -3,3 +3,28 @@ const Hangman = function(word, remainingGuesses) {
   this.remainingGuesses = remainingGuesses;
   this.guessedLetters = [];
 };
+
+Hangman.prototype.getPuzzle = function() {
+  let puzzle = '';
+  this.word.forEach(letter => {
+    if (this.guessedLetters.includes(letter) || letter === ' ') {
+      puzzle += letter;
+    } else {
+      puzzle += '*';
+    }
+  });
+  return puzzle;
+};
+
+Hangman.prototype.makeGuess = function(guess) {
+  guess = guess.toLowerCase();
+  const isUnique = !this.guessedLetters.includes(guess);
+  const isBadGuess = !this.word.includes(guess);
+
+  if (isUnique) {
+    this.guessedLetters.push(guess);
+  }
+  if (isUnique && isBadGuess) {
+    this.remainingGuesses--;
+  }
+};
