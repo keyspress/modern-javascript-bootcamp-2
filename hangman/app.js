1a49459490419ecc8312f4b7767fc5f5fa1eb912
Kyle Desktop <kylespresser@gmail.com>
2018-10-02 09:05:44 -0400

    lecture 97, challenge 1 my solution

--- hangman/app.js
+++ hangman/app.js
@@ -1,7 +1,46 @@
-const product = 'string';
+Hangman.prototype.getPuzzle = function() {
+  let puzzle = '';
+  this.word.forEach(letter => {
+    if (this.guessedLetters.includes(letter) || letter === ' ') {
+      puzzle += letter;
+    } else {
+      puzzle += '*';
+    }
+  });
+  return puzzle;
+};
 
-console.log(product);
+Hangman.prototype.makeGuess = function(guess) {
+  guess = guess.toLowerCase();
+  const isUnique = !this.guessedLetters.includes(guess);
+  const isBadGuess = !this.word.includes(guess);
 
-const otherProduct = new String('phone');
+  if (isUnique) {
+    this.guessedLetters.push(guess);
+  }
+  if (isUnique && isBadGuess) {
+    this.remainingGuesses--;
+  }
+};
 
-console.log(otherProduct);
+const game1 = new Hangman('dog', 3);
+
+const generatePuzzleDOM = () => {
+  const puzzleEl = document.createElement('h2');
+  const remainingGuessEl = document.createElement('h4');
+  puzzleEl.textContent = game1.getPuzzle();
+  remainingGuessEl.textContent = game1.remainingGuesses;
+  document.querySelector('#puzzle').appendChild(puzzleEl);
+  document.querySelector('#puzzle').appendChild(remainingGuessEl);
+};
+
+generatePuzzleDOM();
+
+window.addEventListener('keypress', e => {
+  const guess = String.fromCharCode(e.charCode);
+  game1.makeGuess(guess);
+  document.querySelector('#puzzle').innerHTML = '';
+  generatePuzzleDOM();
+  // console.log(game1.getPuzzle());
+  // console.log(game1.remainingGuesses);
+});
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -3,37 +3,3 @@ const Hangman = function(word, remainingGuesses) {
   this.remainingGuesses = remainingGuesses;
   this.guessedLetters = [];
 };
-
-Hangman.prototype.getPuzzle = function() {
-  let puzzle = '';
-  this.word.forEach(letter => {
-    if (this.guessedLetters.includes(letter) || letter === ' ') {
-      puzzle += letter;
-    } else {
-      puzzle += '*';
-    }
-  });
-  return puzzle;
-};
-
-Hangman.prototype.makeGuess = function(guess) {
-  guess = guess.toLowerCase();
-  const isUnique = !this.guessedLetters.includes(guess);
-  const isBadGuess = !this.word.includes(guess);
-
-  if (isUnique) {
-    this.guessedLetters.push(guess);
-  }
-  if (isUnique && isBadGuess) {
-    this.remainingGuesses--;
-  }
-};
-
-const game1 = new Hangman('dog', 3);
-
-window.addEventListener('keypress', e => {
-  const guess = String.fromCharCode(e.charCode);
-  game1.makeGuess(guess);
-  console.log(game1.getPuzzle());
-  console.log(game1.remainingGuesses);
-});
