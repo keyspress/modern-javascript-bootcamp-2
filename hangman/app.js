37016181ce61f616cd881819dd3f817ef2ce856e
Kyle Desktop <kylespresser@gmail.com>
2018-10-02 12:44:42 -0400

    lecture 104

--- hangman/app.js
+++ hangman/app.js
@@ -12,3 +12,15 @@ window.addEventListener('keypress', e => {
   puzzleEl.textContent = game1.puzzle;
   guessesEl.textContent = game1.statusMessage;
 });
+
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', e => {
+  if (e.target.readyState === 4) {
+    const data = JSON.parse(e.target.responseText);
+    console.log(data);
+  }
+});
+
+request.open('GET', 'http://puzzle.mead.io/puzzle');
+request.send();
