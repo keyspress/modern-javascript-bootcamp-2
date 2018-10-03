2e8488d1f82053594f7ed83e70c21cddfbbaf217
Kyle Desktop <kylespresser@gmail.com>
2018-10-03 14:34:20 -0400

    lecture 115

--- hangman/requests.js
+++ hangman/requests.js
@@ -21,26 +21,5 @@ const getCountry = countryCode => {
         throw new Error('Unable to fetch data');
       }
     })
-    .then(data => {
-      const country = data.find(country => country.alpha2Code === countryCode);
-      return country;
-    });
+    .then(data => data.find(country => country.alpha2Code === countryCode));
 };
-// new Promise((resolve, reject) => {
-//   const countryRequest = new XMLHttpRequest();
-
-//   countryRequest.addEventListener('readystatechange', e => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//       const data = JSON.parse(e.target.responseText);
-//       const country = data.find(
-//         country => country.alpha2Code === countryCode
-//       );
-//       resolve(country);
-//     } else if (e.target.readyState === 4) {
-//       reject('error');
-//     }
-//   });
-
-//   countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
-//   countryRequest.send();
-// });
