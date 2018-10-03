bd13965611d6d8de6297b27374afb7231b9850b7
Kyle Desktop <kylespresser@gmail.com>
2018-10-03 09:04:42 -0400

    lecture 107

--- hangman/app.js
+++ hangman/app.js
@@ -13,34 +13,26 @@ window.addEventListener('keypress', e => {
   guessesEl.textContent = game1.statusMessage;
 });
 
-const request = new XMLHttpRequest();
-
-request.addEventListener('readystatechange', e => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText);
-    console.log(data);
-  } else if (e.target.readyState === 4) {
-    console.log('error');
+const puzzle = getPuzzle((error, puzzle) => {
+  if (error) {
+    console.log(`Error: ${error}`);
+  } else {
+    console.log(puzzle);
   }
 });
 
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3');
-request.send();
+// const countryRequest = new XMLHttpRequest();
+// const countryCode = 'US';
 
-const countryRequest = new XMLHttpRequest();
-const countryCode = 'US';
+// countryRequest.addEventListener('readystatechange', e => {
+//   if (e.target.readyState === 4 && e.target.status === 200) {
+//     const data = JSON.parse(e.target.responseText);
+//     const country = data.find(country => country.alpha2Code === countryCode);
+//     console.log(country.name);
+//   } else if (e.target.readyState === 4) {
+//     console.log('error');
+//   }
+// });
 
-countryRequest.addEventListener('readystatechange', e => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText);
-    const country = data.find(country => country.alpha2Code === countryCode);
-    // const country = data.filter(country => country.alpha2Code === countryCode);
-    // console.log(country[0].name);
-    console.log(country.name);
-  } else if (e.target.readyState === 4) {
-    console.log('error');
-  }
-});
-
-countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
-countryRequest.send();
+// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
+// countryRequest.send();
