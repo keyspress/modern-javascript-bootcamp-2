2618ab4eaeead3e3c56853cdb1b6acb09da41514
Kyle Desktop <kylespresser@gmail.com>
2018-10-03 11:53:25 -0400

    lecture 109 challenge, my solution

--- hangman/app.js
+++ hangman/app.js
@@ -13,7 +13,7 @@ window.addEventListener('keypress', e => {
   guessesEl.textContent = game1.statusMessage;
 });
 
-getPuzzle((error, puzzle) => {
+getPuzzle('3', (error, puzzle) => {
   if (error) {
     console.log(`Error: ${error}`);
   } else {
@@ -21,18 +21,10 @@ getPuzzle((error, puzzle) => {
   }
 });
 
-// const countryRequest = new XMLHttpRequest();
-// const countryCode = 'US';
-
-// countryRequest.addEventListener('readystatechange', e => {
-//   if (e.target.readyState === 4 && e.target.status === 200) {
-//     const data = JSON.parse(e.target.responseText);
-//     const country = data.find(country => country.alpha2Code === countryCode);
-//     console.log(country.name);
-//   } else if (e.target.readyState === 4) {
-//     console.log('error');
-//   }
-// });
-
-// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
-// countryRequest.send();
+getCountry('US', (error, country) => {
+  if (error) {
+    console.log(`Error: ${error}`);
+  } else {
+    console.log(country.name);
+  }
+});
