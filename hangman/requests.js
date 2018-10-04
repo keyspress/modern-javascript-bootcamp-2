f8da3b78f4b722b3e32a2846a4a8efe7d514f573
Kyle Desktop <kylespresser@gmail.com>
2018-10-04 09:18:10 -0400

    lecture 118 challenge1, my solution

--- hangman/requests.js
+++ hangman/requests.js
@@ -1,18 +1,26 @@
-const getPuzzle = wordCount => {
-  return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
-    .then(response => {
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        throw new Error('Unable to fetch puzzle');
-      }
-    })
-    .then(data => {
-      return data.puzzle;
-    });
+const getPuzzle = async wordCount => {
+  const response = await fetch(
+    `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`
+  );
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.puzzle;
+  } else {
+    throw new Error('Unable to fetch puzzle');
+  }
 };
 
-const getCountry = countryCode => {
+const getCountry = async countryCode => {
+  const response = await fetch('http://restcountries.eu/rest/v2/all');
+  if (response.status === 200) {
+    let data = await response.json();
+    return data.find(country => country.alpha2Code === countryCode);
+  } else {
+    throw new Error('Unable to fetch data');
+  }
+};
+
+const getCountryOld = countryCode => {
   return fetch('http://restcountries.eu/rest/v2/all')
     .then(response => {
       if (response.status === 200) {
@@ -24,7 +32,16 @@ const getCountry = countryCode => {
     .then(data => data.find(country => country.alpha2Code === countryCode));
 };
 
-const getLocation = () => {
+const getLocation = async () => {
+  const response = await fetch('http://ipinfo.io/json?token=386f858124da7e');
+  if (response.status === 200) {
+    return await response.json();
+  } else {
+    throw new Error('Unable to fetch data');
+  }
+};
+
+const getLocationOld = () => {
   return fetch('http://ipinfo.io/json?token=386f858124da7e').then(response => {
     if (response.status === 200) {
       return response.json();
