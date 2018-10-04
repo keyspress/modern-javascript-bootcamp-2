5004a578c22ca21ad6aab650d43552226ca238e1
Kyle Desktop <kylespresser@gmail.com>
2018-10-04 11:02:49 -0400

    lecture 119

--- hangman/app.js
+++ hangman/app.js
@@ -1,30 +1,41 @@
 const puzzleEl = document.querySelector('#puzzle');
 const guessesEl = document.querySelector('#guesses');
-const game1 = new Hangman('Car Parts', 3);
-
-puzzleEl.textContent = game1.puzzle;
-guessesEl.textContent = game1.statusMessage;
+let game1;
 
 window.addEventListener('keypress', e => {
   const guess = String.fromCharCode(e.charCode);
   game1.makeGuess(guess);
   document.querySelector('#puzzle').innerHTML = '';
-  puzzleEl.textContent = game1.puzzle;
-  guessesEl.textContent = game1.statusMessage;
+  render();
 });
 
-getPuzzle('3')
-  .then(puzzle => {
-    console.log(puzzle);
-  })
-  .catch(err => {
-    console.log(`Error: ${err}`);
-  });
+const render = () => {
+  puzzleEl.textContent = game1.puzzle;
+  guessesEl.textContent = game1.statusMessage;
+};
 
-getCurrentCountry()
-  .then(country => {
-    console.log(country.name);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+const startGame = async () => {
+  const puzzle = await getPuzzle('2');
+  game1 = new Hangman(puzzle, 5);
+  render();
+};
+
+document.querySelector('#reset').addEventListener('click', startGame);
+
+startGame();
+
+// getPuzzle('3')
+//   .then(puzzle => {
+//     console.log(puzzle);
+//   })
+//   .catch(err => {
+//     console.log(`Error: ${err}`);
+//   });
+
+// getCurrentCountry()
+//   .then(country => {
+//     console.log(country.name);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
