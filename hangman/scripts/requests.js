257fd53b817e6792d58d21f6024a33553ae4eac7
Kyle Desktop <kylespresser@gmail.com>
2018-10-04 13:15:12 -0400

    lecture 127

--- hangman/scripts/requests.js
+++ hangman/scripts/requests.js
@@ -1,6 +1,6 @@
 const getPuzzle = async wordCount => {
   const response = await fetch(
-    `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`
+    `//puzzle.mead.io/puzzle?wordCount=${wordCount}`
   );
   if (response.status === 200) {
     const data = await response.json();
@@ -11,7 +11,7 @@ const getPuzzle = async wordCount => {
 };
 
 const getCountry = async countryCode => {
-  const response = await fetch('http://restcountries.eu/rest/v2/all');
+  const response = await fetch('//restcountries.eu/rest/v2/all');
   if (response.status === 200) {
     let data = await response.json();
     return data.find(country => country.alpha2Code === countryCode);
@@ -21,7 +21,7 @@ const getCountry = async countryCode => {
 };
 
 const getLocation = async () => {
-  const response = await fetch('http://ipinfo.io/json?token=386f858124da7e');
+  const response = await fetch('//ipinfo.io/json?token=386f858124da7e');
   if (response.status === 200) {
     return response.json();
   } else {
