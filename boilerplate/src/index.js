eb88966b1d4785f846c3adf4c4168e6fafa19012
Kyle Desktop <kylespresser@gmail.com>
2018-10-05 11:12:11 -0400

    lecture 135

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,10 +1,6 @@
 import { add, name } from './utilities';
 import { scream } from './scream';
 
-console.log('index');
-
 console.log(add(2, 5));
-
 console.log(name);
-
 console.log(scream('shit'));
