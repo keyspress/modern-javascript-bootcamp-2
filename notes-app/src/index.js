996a338cec0aaffb60eb82a92ee6536890eb606a
Kyle Desktop <kylespresser@gmail.com>
2018-10-05 15:19:43 -0400

    lecture 144

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,5 +1,18 @@
-import { getNotes, createNote } from './notes';
+import { getNotes, createNote, removeNote, updateNote } from './notes';
+import { getFilters, setFilters } from './filters';
 
-console.log(getNotes());
-createNote();
-console.log(getNotes());
+// console.log(getNotes());
+// createNote();
+// removeNote('e77da467-5df7-4cfa-8bda-f0ce4c6d0553');
+// updateNote('dea4a5cd-79c0-41c2-83ea-cbce6bb4af2a', {
+//   title: 'note title',
+//   body: 'text'
+// });
+// console.log(getNotes());
+
+console.log(getFilters());
+setFilters({
+  searchText: 'Office',
+  sortBy: 'byCreated'
+});
+console.log(getFilters());
--- notes-app/src/notes.js
+++ notes-app/src/notes.js
@@ -32,6 +32,71 @@ const createNote = () => {
   saveNotes();
 };
 
+const removeNote = id => {
+  const noteIndex = notes.findIndex(note => note.id === id);
+
+  if (noteIndex > -1) {
+    notes.splice(noteIndex, 1);
+    saveNotes();
+  }
+};
+
+const sortNotes = sortBy => {
+  if (sortBy === 'byEdited') {
+    return notes.sort((a, b) => {
+      if (a.updatedAt > b.updatedAt) {
+        return -1;
+      } else if (a.updatedAt < b.updatedAt) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else if (sortBy === 'byCreated') {
+    return notes.sort((a, b) => {
+      if (a.createdAt > b.createdAt) {
+        return -1;
+      } else if (a.createdAt < b.createdAt) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else if (sortBy === 'alphabetical') {
+    return notes.sort((a, b) => {
+      if (a.title.toLowerCase() < b.title.toLowerCase()) {
+        return -1;
+      } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else {
+    return notes;
+  }
+};
+
+const updateNote = (id, updates) => {
+  const note = notes.find(note => note.id === id);
+
+  if (!note) {
+    return;
+  }
+
+  if (typeof updates.title === 'string') {
+    note.title = updates.title;
+    note.updatedAt = moment().valueOf();
+  }
+
+  if (typeof updates.body === 'string') {
+    note.body = updates.body;
+    note.updatedAt = moment().valueOf();
+  }
+
+  saveNotes();
+};
+
 notes = loadNotes();
 
-export { getNotes, createNote };
+export { getNotes, createNote, removeNote, sortNotes, updateNote };
