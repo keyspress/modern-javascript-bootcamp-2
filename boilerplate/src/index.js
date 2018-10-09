e3b709b0f40456e9dee546210db401daa0f2e46d
Kyle Desktop <kylespresser@gmail.com>
2018-10-09 11:39:57 -0400

    lecture 151

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -16,3 +16,34 @@ let cities = ['Barcelona', 'Cape Town', 'Bordeaux'];
 cities = ['Miami', ...cities, 'Santiago'];
 
 console.log(cities);
+
+let house = {
+  bedrooms: 2,
+  bathrooms: 1.5,
+  yearBuilt: 2000
+};
+
+let newHouse = {
+  basement: true,
+  ...house,
+  bedrooms: 3
+};
+
+console.log(house);
+console.log(newHouse);
+
+const person = {
+  name: 'Jim',
+  age: 34
+};
+
+const location = {
+  city: 'Tampa',
+  country: 'US'
+};
+
+const overview = {
+  ...person,
+  ...location
+};
+console.log(overview);
